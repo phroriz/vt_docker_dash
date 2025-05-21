b85bab1b141ed73407e59d60d9f6d65401b3228c
Pedro Roriz <40444217+phroriz@users.noreply.github.com>
2025-05-21 17:12:04 -0300

    Update wait-for-mysql.sh

--- docker/php/wait-for-mysql.sh
+++ docker/php/wait-for-mysql.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
-set -e
-
 echo "⏳ Aguardando o MySQL iniciar em $DB_HOST:$DB_PORT..."
 
-# Loop até conseguir conectar ao MySQL
-until nc -z -v -w30 "$DB_HOST" "$DB_PORT"; do
+until nc -z -v -w30 $DB_HOST $DB_PORT
+do
   echo "🔁 Aguardando conexão com o banco..."
   sleep 3
 done
 
 echo "✅ Banco disponível! Iniciando Apache..."
-
-# Instala dependências do Composer, se necessário
-if [ -f "composer.json" ]; then
-  echo "📦 Instalando dependências do Composer..."
-  composer install --no-interaction --prefer-dist
-fi
-
-# Executa o Apache em foreground
 exec apache2-foreground
